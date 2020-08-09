17306959d9acf62b2ddf2ef0a93db05839661ec1
SalmaYassin <salmayassin.1997@gmail.com>
2020-08-09 20:54:14 +0200

    remove graph

--- app/src/main/java/com/example/dokkanseller/views/MainActivity.java
+++ app/src/main/java/com/example/dokkanseller/views/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.dokkanseller.views;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.dokkanseller.R;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
 }
+
