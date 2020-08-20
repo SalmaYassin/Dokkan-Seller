29d4a43f57cbddbbef8e817afdf26e42d1f1392e
SalmaYassin <salmayassin.1997@gmail.com>
2020-08-20 18:50:21 +0200

    adding order details design

---
+++ app/src/main/java/com/example/dokkanseller/views/orders/OrderDetailsFragment/OrderDetailsFragment.java
@@ -0,0 +1,41 @@
+package com.example.dokkanseller.views.orders.OrderDetailsFragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.dokkanseller.R;
+import com.example.dokkanseller.views.base.BaseFragment;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class OrderDetailsFragment extends BaseFragment {
+
+
+    public OrderDetailsFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public int getLayoutId() {
+        return R.layout.fragment_order_details;
+    }
+
+    @Override
+    public void initializeViews(View view) {
+
+    }
+
+    @Override
+    public void setListeners() {
+
+    }
+}
--- app/src/main/java/com/example/dokkanseller/views/orders/allOrders/AllOrdersAdapter.java
+++ app/src/main/java/com/example/dokkanseller/views/orders/allOrders/AllOrdersAdapter.java
@@ -4,15 +4,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.dokkanseller.R;
 import com.example.dokkanseller.data_model.OrderItemModel;
+import com.squareup.picasso.Picasso;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -35,14 +34,14 @@ public class AllOrdersAdapter extends RecyclerView.Adapter<AllOrdersAdapter.AllO
     @Override
     public void onBindViewHolder(@NonNull AllOrdersVHolder holder, int position) {
 
-         holder.productName.setText(orderItemModels.get(position).getCartItem().get(position).productName);
 
-         holder.productQuantity.setText(orderItemModels.get(position).getCartItem().get(position).getProductQuanitity()+" ");
-         // picaso for productImage
+         // picaso for orderDate
+        Picasso.get().load(""/*todo add customer photo*/).placeholder(R.drawable.icon4).into(holder.customerImage);
         holder.customerName.setText(orderItemModels.get(position).getAddress().getCustomerName());
         holder.customerPhoneNum.setText(orderItemModels.get(position).getAddress().getCustomerNumber());
         String address = orderItemModels.get(position).getAddress().getCustomerCountry() + orderItemModels.get(position).getAddress().customerAddress ;
         holder.customerAddressLocation.setText(address);
+        holder.orderDate.setText(""/*todo add order date*/);
         Log.e("a",orderItemModels.get(position).getCartItem().size()+"");
 
 
@@ -62,30 +61,23 @@ public class AllOrdersAdapter extends RecyclerView.Adapter<AllOrdersAdapter.AllO
 
     public class AllOrdersVHolder extends RecyclerView.ViewHolder {
 
-        private CircleImageView productImage;
-        private TextView productName;
-        private EditText productQuantity;
+        private TextView orderDate;
         private CircleImageView customerImage;
         private TextView customerName;
-        private EditText customerPhoneNum;
-        private EditText customerAddressLocation;
-        private Button acceptBtn;
-        private Button rejectBtn;
+        private TextView customerPhoneNum;
+        private TextView customerAddressLocation;
+
 
         public AllOrdersVHolder(@NonNull View itemView) {
             super(itemView);
             initView(itemView);
         }
         private void initView(@NonNull final View itemView) {
-            productImage = (CircleImageView) itemView.findViewById(R.id.product_image_0);
-            productName = (TextView) itemView.findViewById(R.id.item_name_0);
-            productQuantity = (EditText) itemView.findViewById(R.id.quantity_number_0);
-            customerImage = (CircleImageView) itemView.findViewById(R.id.customer_image_0);
-            customerName = (TextView) itemView.findViewById(R.id.customer_name_0);
-            customerPhoneNum = (EditText) itemView.findViewById(R.id.customer_phone_num_0);
-            customerAddressLocation = (EditText) itemView.findViewById(R.id.customer_address_location_0);
-            acceptBtn = (Button) itemView.findViewById(R.id.accept_btn);
-            rejectBtn = (Button) itemView.findViewById(R.id.reject_btn);
+            orderDate =  itemView.findViewById(R.id.tvOrderDateValue);
+            customerAddressLocation = (TextView) itemView.findViewById(R.id.tvCustomerAdress);
+            customerImage = (CircleImageView) itemView.findViewById(R.id.imgCustomerProfile);
+            customerName = (TextView) itemView.findViewById(R.id.tvCustomerName);
+            customerPhoneNum = (TextView) itemView.findViewById(R.id.tvCustomerPhone);
         }
     }
 }
